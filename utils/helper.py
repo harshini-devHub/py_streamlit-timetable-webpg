49a9a9e3929e842ae604e87e0ba2150fbb4c0049
harshini-devHub <harshinikurapati@outlook.com>
2024-07-25 21:56:52 +0100

    final json timetable

---
+++ utils/helper.py
@@ -0,0 +1,13 @@
+#
+def get_timetable(s_class: str, date: str) -> dict:
+    """this sub-routine takes user's class and date and outputs week and subjects for the day
+    Args:
+        s_class (str): Students class name
+        date (str): date (YYYY-MM-DD) for which the timetable is needed.
+
+    Returns:
+        dict: Week type and subjects {"week": "Week A", "subjects": ["Maths", "Sci", "PE"]}
+    """
+    output: dict = {}
+
+    return output
